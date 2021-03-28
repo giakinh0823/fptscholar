6e370ba6243eef42212177812b92512717e46531
Gia Kinh <giakinh2000@gmail.com>
2021-03-28 10:22:12 +0700

    done local remote selenium

--- Scholar/getDataScholar.py
+++ Scholar/getDataScholar.py
@@ -75,6 +75,25 @@ import urllib.request as urllib2
 from django.core.files import File
 from django.core.files.base import ContentFile
 import io
+from selenium.webdriver.common.keys import Keys
+
+
+#https://automation.lambdatest.com/
+
+username = "giakinhfullstack" # Replace the username
+access_key = "pmkWJcII2jVdEf4zDM1kUzz05gyEY9qu1JVQ9lFwHtyWiWdQI1" # Replace the access key
+
+desired_caps = {
+            "build": 'PyunitTest sample build', # Change your build name here
+            "name": 'Py-unittest', # Change your test name here
+            "platform": 'Windows 10', # Change your OS version here
+            "browserName": 'Chrome', # Change your browser here
+            "version": '87.0', # Change your browser version here
+            "resolution": '1024x768', # Change your resolution here
+            "console": 'true', # Enable or disable console logs
+            "network":'true'   # Enable or disable network logs
+        }
+
 
 
 from article.models import Article
@@ -103,11 +122,14 @@ def data_profile(link):
     # options.add_argument('window-size=1920x1080')
     # options.add_argument("disable-gpu")
     
-    options = webdriver.ChromeOptions() 
-    options.add_argument("start-maximized")
+    # options = webdriver.ChromeOptions() 
+    # options.add_argument("start-maximized")
     # options.add_argument('disable-infobars')
     
-    driver = webdriver.Chrome(ChromeDriverManager().install(),options=options,)
+    # driver = webdriver.Chrome(ChromeDriverManager().install(),options=options,)
+    driver = webdriver.Remote(
+            command_executor="https://{}:{}@hub.lambdatest.com/wd/hub".format(username, access_key),
+            desired_capabilities= desired_caps)
     # driver = webdriver.Edge(EdgeChromiumDriverManager().install())
     driver.get(str(link))
     # driver.execute_script("arguments[0].click();", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']/div[@class='recaptcha-checkbox-checkmark']"))))
@@ -202,7 +224,9 @@ def data_profile(link):
     # file_name ='profile'+ datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + ".csv"
     # df.to_csv(file_name, index=False)
 
-    
+
+
+
 
 def data_scrap(link,user):
     # Empty lists for storing information
@@ -245,14 +269,22 @@ def data_scrap(link,user):
     # options.add_argument('headless')
     # options.add_argument('window-size=1920x1080')
     # options.add_argument("disable-gpu")
-    
-    options = webdriver.ChromeOptions() 
+
+    # options = webdriver.ChromeOptions() 
+    options = webdriver.FirefoxOptions()
     # options.add_argument("start-maximized")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument('--disable-dev-shm-usage')
-    options.headless = True
-    driver = webdriver.Chrome(ChromeDriverManager(version="90.0.4430.24",chrome_type=ChromeType.CHROMIUM).install(),  chrome_options=options)
+    
+    # options.add_argument("--headless")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.headless = True
+    
+    # driver = webdriver.Chrome(ChromeDriverManager(version="90.0.4430.24",chrome_type=ChromeType.CHROMIUM).install(),  chrome_options=options)
+    
+    
+    driver = webdriver.Remote(
+            command_executor="https://{}:{}@hub.lambdatest.com/wd/hub".format(username, access_key),
+            desired_capabilities= desired_caps)
     driver.get(str(link))
     time.sleep(1)
 
