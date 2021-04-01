99ebed603836c6d00b22fdc359fc864015b5e50b
Kính Hà Gia <giakinh2000@gmail.com>
2021-04-01 11:04:09 +0700

    587 commit

--- Scholar/settings.py
+++ Scholar/settings.py
@@ -84,7 +84,7 @@ INSTALLED_APPS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_POST = '587'
+EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'giakinhfullstack@gmail.com'
 EMAIL_HOST_PASSWORD = 'giakinh0823'
